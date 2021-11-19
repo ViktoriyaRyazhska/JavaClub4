52161f8ffb2e5df86b838301990b94a23692880b
Tetyana Yahodska <yahodska@gmail.com>
2021-11-19 19:24:46 +0200

    Create task5  of stage basic

---
+++ src/tasks/Task5.java
@@ -0,0 +1,14 @@
+package tasks;
+
+public class Task5 {
+    public void execute (String name ){
+
+        System.out.println(greet(name));
+    }
+    public static String greet(String name) {
+        if(name.equals("Johnny")){
+            return "Hello, my love!";
+        }else{
+            return String.format("Hello, %s!", name);}
+    }
+}
