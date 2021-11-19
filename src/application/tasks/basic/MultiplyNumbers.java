f4138ddceaf6044f1ffe7718124a673110d3c547
Roman Kaniuka <kanuka878@gmail.com>
2021-11-19 23:02:51 +0200

    Added task "MultiplyNumbers" to package "basic"

---
+++ src/application/tasks/basic/MultiplyNumbers.java
@@ -0,0 +1,17 @@
+package application.tasks.basic;
+
+import java.util.Scanner;
+
+public class MultiplyNumbers {
+    private static int multiply(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public static void task() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter two numbers to multiply: ");
+        int number1 = in.nextInt();
+        int number2 = in.nextInt();
+        System.out.println("Result: " + multiply(number1, number2));
+    }
+}
