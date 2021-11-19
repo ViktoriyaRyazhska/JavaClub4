c26c49ecfda8b6eb31954c7c1c2d63f0d56d216a
Roman Kaniuka <kanuka878@gmail.com>
2021-11-19 22:38:09 +0200

    Added console menu to main. Added task "UnderPressure" to menu

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,39 @@
 package application;
 
-import tasks.basic.UnderPressure;
+import application.tasks.basic.UnderPressure;
+
+import java.util.Scanner;
 
 public class Main {
+    private static void choice() {
+        System.out.print("\nSelect an option: ");
+        Scanner in = new Scanner(System.in);
+        int option = in.nextInt();
+
+        switch (option) {
+            default:
+                System.out.println("Sorry.. this option don`t exist");
+                break;
+            case 0:
+                break;
+            case 1:
+                UnderPressure.task();
+                choice();
+                break;
+
+        }
+    }
+    private static String listOfOptions() {
+        String listOfOptions =
+                "0. Exit\n" +
+                "1. Double your number\n" +
+                "";
+
+        return listOfOptions;
+    }
+
     public static void main(String[] args) {
-        UnderPressure one = new UnderPressure();
-        one.one();
+        System.out.println(listOfOptions());
+        choice();
     }
 }
