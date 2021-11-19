e6f36b9b3c431b6f82ad34b230ddf6585e564671
Roman Kaniuka <kanuka878@gmail.com>
2021-11-20 01:57:24 +0200

    Modified Main with dividing menu into sections, adding task "OppositeNumber" to menu

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,12 +4,40 @@ import application.tasks.basic.AgeRange;
 import application.tasks.basic.CuboidVolume;
 import application.tasks.basic.MultiplyNumbers;
 import application.tasks.basic.UnderPressure;
+import application.tasks.conditions.OppositeNumber;
 
 import java.util.Scanner;
 
 public class Main {
-    private static void choice() {
-        System.out.print("\nSelect an option: ");
+    private static void choiceList() {
+        System.out.print("Select a list of tasks:\n> ");
+        Scanner in = new Scanner(System.in);
+        int option = in.nextInt();
+
+        switch (option) {
+            default:
+                System.out.println("Sorry.. this option don`t exist");
+            case 0:
+                break;
+            case 1:
+                choiceBasic();
+                break;
+            case 2:
+                choiceConditions();
+                break;
+        }
+    }
+    private static String listOfLists() {
+        String lists =
+                "1. Basic\n" +
+                "2. Conditions\n";
+
+        return lists;
+    }
+
+    private static void choiceBasic() {
+        System.out.println(listOfBasicOptions());
+        System.out.print("Select an option:\n> ");
         Scanner in = new Scanner(System.in);
         int option = in.nextInt();
 
@@ -19,37 +47,70 @@ public class Main {
                 break;
             case 0:
                 break;
+            case -1:
+                choiceList();
+                break;
             case 1:
                 UnderPressure.task();
-                choice();
+                choiceBasic();
                 break;
             case 2:
                 MultiplyNumbers.task();
-                choice();
+                choiceBasic();
                 break;
             case 3:
                 CuboidVolume.task();
-                choice();
+                choiceBasic();
                 break;
             case 4:
                 AgeRange.task();
-                choice();
+                choiceBasic();
                 break;
         }
     }
-    private static String listOfOptions() {
-        String listOfOptions =
+    private static String listOfBasicOptions() {
+        String basicOptions = "List of basic tasks:\n\n" +
+                "-1. Back\n" +
                 "0. Exit\n" +
                 "1. Double your number\n" +
                 "2. Multiply your two numbers\n" +
                 "3. Calculate volume of cuboid\n" +
-                "4. Age of your love";
+                "4. Age of your love\n";
 
-        return listOfOptions;
+        return basicOptions;
+    }
+
+    private static void choiceConditions() {
+        System.out.println(listOfConditionsOptions());
+        System.out.print("Select an option:\n> ");
+        Scanner in = new Scanner(System.in);
+        int option = in.nextInt();
+
+        switch (option) {
+            default:
+                System.out.println("Sorry.. this option don`t exist");
+                break;
+            case 0:
+                break;
+            case -1:
+                choiceList();
+                break;
+            case 1:
+                OppositeNumber.task();
+                choiceConditions();
+        }
+    }
+    private static String listOfConditionsOptions() {
+        String conditionsOptions = "List of conditions tasks:\n\n" +
+                "-1. Back\n" +
+                "0. Exit\n" +
+                "1. Make your number opposite\n";
+
+        return conditionsOptions;
     }
 
     public static void main(String[] args) {
-        System.out.println(listOfOptions());
-        choice();
+        System.out.println(listOfLists());
+        choiceList();
     }
 }
