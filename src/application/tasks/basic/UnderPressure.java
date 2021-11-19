1d4c6186f85f63259725bae32070c8cbd9f78f2d
Roman Kaniuka <kanuka878@gmail.com>
2021-11-19 22:36:14 +0200

    Added package "tasks" into package "application". Added task "UnderPressure"

---
+++ src/application/tasks/basic/UnderPressure.java
@@ -0,0 +1,16 @@
+package application.tasks.basic;
+
+import java.util.Scanner;
+
+public class UnderPressure {
+    private static int doubleInteger(int number) {
+        return number * 2;
+    }
+
+    public static void task() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number to double: ");
+        int number = in.nextInt();
+        System.out.println("Result: " + doubleInteger(number));
+    }
+}
