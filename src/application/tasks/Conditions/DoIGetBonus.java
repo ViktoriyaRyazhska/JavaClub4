2a8e8e1ae75d59dbff7fe0b8a9e7796a3e3e37ad
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-20 12:24:41 +0200

    added task "DoIGetBonus" to package Conditions.

---
+++ src/application/tasks/Conditions/DoIGetBonus.java
@@ -0,0 +1,12 @@
+package application.tasks.Conditions;
+
+public class DoIGetBonus {
+    public static String bonusTime(final int salary, final boolean bonus) {
+        // show me the code!
+        if(bonus == true)
+            System.out.println(salary * 10);
+        else
+            System.out.println(salary);
+
+    }
+}
