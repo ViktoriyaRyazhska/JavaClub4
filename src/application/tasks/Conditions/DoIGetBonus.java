19ccfc0aed22a8f8523a10098c2c1af62b465e92
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-20 18:52:57 +0200

    added task() method

--- src/application/tasks/Conditions/DoIGetBonus.java
+++ src/application/tasks/Conditions/DoIGetBonus.java
@@ -1,13 +1,21 @@
 package application.tasks.Conditions;
 
+import java.util.Scanner;
+
 public class DoIGetBonus {
     public static String bonusTime(final int salary, final boolean bonus) {
-        // show me the code!
-        String result;
-        if(bonus == true)
-            System.out.println(salary * 10);
+        if (bonus == true)
+            return String.valueOf(salary * 10);
         else
-            System.out.println(salary);
-        return "";
+            return String.valueOf(salary);
+    }
+
+    public static void task() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your salary:\n> ");
+        int salary = in.nextInt();
+        System.out.print("Do you have bonus?(true or false)\n> ");
+        boolean bonus = in.nextBoolean();
+        System.out.println("Your total income is $" + bonusTime(salary, bonus));
     }
 }
--- src/application/tasks/basic/PixelArtPlanning.java
+++ src/application/tasks/basic/PixelArtPlanning.java
@@ -1,5 +1,7 @@
 package application.tasks.basic;
 
+import java.util.Scanner;
+
 public class PixelArtPlanning {
 
     public static boolean isDivisible(int wallLength, int pixelSize) {
@@ -10,5 +12,17 @@ public class PixelArtPlanning {
             return true;
         else
             return false;
+    }
+    public static void task() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the size of the wall in millimeters:\n> ");
+        int wallLength = in.nextInt();
+        System.out.print("Enter the size of a pixel in millimeters:\n> ");
+        int pixelSize = in.nextInt();
+
+        if(isDivisible(wallLength,pixelSize) ==  true)
+            System.out.println("You can fit an exact number of pixels on the given wall");
+        else
+            System.out.println("You can't fit an exact number of pixels on the given wall");
     }
 }
