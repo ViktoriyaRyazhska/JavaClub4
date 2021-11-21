2e0c1c1c3eb3cff97b02a706db2ec59c1acc6389
Roman Kaniuka <kanuka878@gmail.com>
2021-11-22 00:37:41 +0200

    Added task "Encapsulation" to new package 'classes'

---
+++ src/application/tasks/classes/Encapsulation.java
@@ -0,0 +1,38 @@
+package application.tasks.classes;
+
+public class Encapsulation {
+    private int number;
+    private String stringValue;
+    private Object anObject;
+
+    Encapsulation() {};
+    Encapsulation(int number, String stringValue, Object anObject) {
+        this.number = number;
+        this.stringValue = stringValue;
+        this.anObject = anObject;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public Object getAnObject() {
+        return anObject;
+    }
+
+    public void setAnObject(Object anObject) {
+        this.anObject = anObject;
+    }
+}
