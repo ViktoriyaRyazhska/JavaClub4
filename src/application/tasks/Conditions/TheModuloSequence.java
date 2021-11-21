ba08f38b1eea58557ae6a6bc515b6b0ef22e5b30
Roman Kaniuka <kanuka878@gmail.com>
2021-11-21 14:47:21 +0200

    Added task "TheModuloSequence" to package 'conditions'

---
+++ src/application/tasks/Conditions/TheModuloSequence.java
@@ -0,0 +1,29 @@
+package application.tasks.conditions;
+
+import java.util.Scanner;
+
+public class TheModuloSequence {
+    private static int findNth(int n) {
+        int[] sequence = new int[n + 1];
+        sequence[0] = 0;
+        sequence[1] = 1;
+        for (int i = 2; i < n; i++) {
+            sequence[i] = (sequence[i - 1] + sequence[i - 2]) % 3;
+        }
+
+        return sequence[n - 1];
+    }
+
+    public static void task() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the index of number to find:\n> ");
+        int n = in.nextInt();
+        if (n < 1) {
+            System.out.println("Error.. invalid input");
+            task();
+        } else {
+            System.out.println("Result: " + findNth(n));
+        }
+    }
+}
+
