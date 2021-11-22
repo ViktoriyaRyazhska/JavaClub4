5527b8f137a20a474905dfc32e3aa9ecde578333
Roman Kaniuka <kanuka878@gmail.com>
2021-11-22 02:04:32 +0200

    Added task "Summation" to package 'loops'

---
+++ src/application/tasks/loops/Summation.java
@@ -0,0 +1,21 @@
+package application.tasks.loops;
+
+import java.util.Scanner;
+
+public class Summation {
+    private static int summation(int n) {
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+
+        return result;
+    }
+
+    public static void task() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter amount of number to sum:\n> ");
+        int n = in.nextInt();
+        System.out.println("Result: " + summation(n));
+    }
+}
