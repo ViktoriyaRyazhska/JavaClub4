32265412d62dc1f32ab89a4db6ed341afae190bf
yura-stefanyshyn <yuristefanyshyn@gmail.com>
2021-11-23 16:39:54 +0200

    fixed OppositesAttract

--- src/application/tasks/conditions/OppositesAttract.java
+++ src/application/tasks/conditions/OppositesAttract.java
@@ -1,7 +1,7 @@
 package application.tasks.Conditions;
 
 public class OppositesAttract {
-    private static boolean task(final int flower1, final int flower2) {
+    public static boolean task(final int flower1, final int flower2) {
             boolean point =false;
             if(flower1%2==0 & flower2%2==1 ^ flower1%2==1 & flower2%2==0){
                 point = true;
