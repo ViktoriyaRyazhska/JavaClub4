7a15bb10b4499a638bef6044d721e1dc4028e0e3
marjash <marjash@i.ua>
2021-11-23 20:29:09 +0200

    created traffic lights

---
+++ src/Conditions/TrafficLights.java
@@ -0,0 +1,11 @@
+public class TrafficLights {
+    public static String updateLight(String current) {
+        if (current.equals("green"))
+          return "yellow";
+        if (current.equals("yellow"))
+          return "red";
+        if (current.equals("red"))
+          return "green";
+        return "";
+    }
+}
