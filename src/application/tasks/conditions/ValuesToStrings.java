c893b8f033b945b7b9fe09936c36ccc4a65a6c1f
yura-stefanyshyn <yuristefanyshyn@gmail.com>
2021-11-23 16:28:49 +0200

    Created new class ValuesToString

---
+++ src/application/tasks/conditions/ValuesToStrings.java
@@ -0,0 +1,19 @@
+package application.tasks.Conditions;
+
+import java.util.Scanner;
+
+public class ValuesToStrings {
+    public static String task(boolean b){
+        String Yes ="Yes";
+        String No ="No";
+        Scanner bool = new Scanner(System.in);
+        System.out.println("Your value is " + b);
+        b = bool.nextBoolean();
+        if(b == true){
+            return Yes;
+        }else
+            return No;
+
+    }
+}
+
