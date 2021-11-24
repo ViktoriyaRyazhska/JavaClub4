2c54c60914d292ac0568ab6ac01563ea8035873c
Vitalii Kr <vitaliikr@gmail.com>
2021-11-24 14:55:18 +0200

    add solve in Basics

---
+++ src/Basics/ThinkfulNumber/ThinkfulNumber.java
@@ -0,0 +1,13 @@
+package Basics.ThinkfulNumber;
+
+/**
+ * You're laying out a rad pixel art mural to paint on your
+ * living room wall in homage to Paul Robertson,
+ * your favorite pixel artist.
+ */
+
+public class ThinkfulNumber {
+    public static boolean isDivisible(int wallLength, int pixelSize) {
+        return true;
+    }
+}
---
+++ src/Basics/YouCantCode/YouCantCode.java
@@ -0,0 +1,12 @@
+package Basics.YouCantCode;
+
+/**
+ * Code as fast as you can! You need to double the integer and return it.
+ */
+public class YouCantCode {
+
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
+
