56cb0d679a0a209ffc1fda7cba814af4fa062249
Roman Kaniuka <kanuka878@gmail.com>
2021-11-24 21:00:14 +0200

    Completed class "ChoiceString"

--- src/application/utility/ChoiceString.java
+++ src/application/utility/ChoiceString.java
@@ -8,46 +8,108 @@ public class ChoiceString extends Constants {
     private static void choiceString(Scanner scanner) {
         System.out.println(listOfString());
         System.out.print("Select an option:\n> ");
-        switch (scanner.nextInt()) {
+        int option = scanner.nextInt();
+        switch (option) {
             default:
                 System.out.println("Sorry.. this option don`t exist");
                 break;
             case EXIT:
                 break;
             case BACK:
-                ChoiceList.choiceList();
+                ChoiceList.choiceList(scanner);
                 break;
             case 1:
-                Banjo.task(scanner);
+                AbbreviateTwoWordName.task(scanner);
                 ChoiceString.choiceString(scanner);
                 break;
             case 2:
-                NumberToString.task(scanner);
+                AllStarCodeChallenge.task(scanner);
                 ChoiceString.choiceString(scanner);
                 break;
             case 3:
-                ReplaceDots.task(scanner);
+                Banjo.task(scanner);
                 ChoiceString.choiceString(scanner);
                 break;
             case 4:
-                ReverseString.task(scanner);
+                CorrectTheMistakes.task(scanner);
                 ChoiceString.choiceString(scanner);
                 break;
             case 5:
+                FakeBinary.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 6:
+                FindThePosition.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 7:
+                GrasshopperDebug.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 8:
+                IntFromChar.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 9:
+                NumberToString.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 10:
+                RemoveExclamationMarks.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 11:
+                RemoveString.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 12:
+                ReplaceDots.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 13:
+                ReverseString.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 14:
+                SortAndStar.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 15:
+                StringUtils.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 16:
+                TripleTrouble.task(scanner);
+                ChoiceString.choiceString(scanner);
+                break;
+            case 17:
                 Welcome.task(scanner);
                 ChoiceString.choiceString(scanner);
                 break;
+
         }
     }
     private static String listOfString() {
         String string = "\nList of of string task:\n" +
                 "-1. Back\n" +
                 "0. Exit\n" +
-                "1. Are you playing banjo\n" +
-                "2. Convert number to String\n" +
-                "3. Replace all dots\n" +
-                "4. Reverse string\n" +
-                "5. Print welcome message";
+                "1. Abbreviate Two Word Name\n" +
+                "2. All Star Code Challenge\n" +
+                "3. Banjo\n" +
+                "4. Correct the Mistakes\n" +
+                "5. Fake Binary\n" +
+                "6. Find the Position\n" +
+                "7. Grasshopper Debug\n" +
+                "8. Int from Char\n" +
+                "9. Number to String\n" +
+                "10. Remove Exclamation Marks\n" +
+                "11. Remove String\n" +
+                "12. Replace Dots\n" +
+                "13. Reverse String\n" +
+                "14. Sort And Star\n" +
+                "15. String Utils\n" +
+                "16. Triple Trouble\n" +
+                "17. Welcome";
 
         return string;
     }
