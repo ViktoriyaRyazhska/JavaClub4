742628446a521970c0823d266a84a24289780359
Roman Kaniuka <kanuka878@gmail.com>
2021-11-24 16:05:03 +0200

    Added task "UnfinishedLoop" to new package 'collections'

---
+++ src/application/tasks/collections/UnfinishedLoop.java
@@ -0,0 +1,23 @@
+package application.tasks.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class UnfinishedLoop {
+    private static List createList(int size) {
+        List list = new ArrayList();
+
+        for(int i = 1; i <= size; i++) {
+            list.add(i);
+        }
+
+        return list;
+    }
+
+    public static void task(Scanner scanner) {
+        System.out.print("Enter size of list:\n> ");
+        int size = scanner.nextInt();
+        System.out.println("Result: " + createList(size));
+    }
+}
