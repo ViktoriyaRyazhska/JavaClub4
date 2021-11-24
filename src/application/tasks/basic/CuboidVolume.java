54317991a73e700116828cb5a4958f522346aff2
Roman Kaniuka <kanuka878@gmail.com>
2021-11-24 12:46:37 +0200

    Modified task "CuboidVolume" with fixing Scanner

--- src/application/tasks/basic/CuboidVolume.java
+++ src/application/tasks/basic/CuboidVolume.java
@@ -7,15 +7,14 @@ public class CuboidVolume {
         return length * height * width;
     }
 
-    public static void task() {
-        Scanner in = new Scanner(System.in);
+    public static void task(Scanner scanner) {
         System.out.print("Enter the length, height and width of cuboid:\n> ");
-        int length = in.nextInt();
-        int height = in.nextInt();
-        int width = in.nextInt();
+        int length = scanner.nextInt();
+        int height = scanner.nextInt();
+        int width = scanner.nextInt();
         if (length <= 0 || height <= 0 || width <= 0) {
             System.out.println("Error.. invalid input");
-            task();
+            task(scanner);
         } else {
             System.out.println("Result: " + volume(length, height, width));
         }
