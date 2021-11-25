93b4aa49c9912c7198ab670bb7e29c7f6fb1f799
Nazar-Spodar <nazar.spodar38@gmail.com>
2021-11-25 21:10:50 +0200

    доробити інтерфейси

---
+++ src/Conditions/OppositeNumber/OppositeNumber.java
@@ -0,0 +1,12 @@
+package Conditions.OppositeNumber;
+
+public class OppositeNumber {
+    public static int oppositeNumber(int number)
+    {
+        return number*(-1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(oppositeNumber(9));
+    }
+}
