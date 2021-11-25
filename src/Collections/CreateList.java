d29257b54e3b8deed4371c3ad08c0bddcaeff4fb
marjash <marjash@i.ua>
2021-11-25 19:36:10 +0200

    created new files

---
+++ src/Collections/CreateList.java
@@ -0,0 +1,12 @@
+class Kata {
+    public static List CreateList(int number) {
+        List list = new ArrayList();
+
+        for (int count = 1; count <= number;) {
+            list.add(count);
+            count++;
+        }
+
+        return list;
+    }
+}
