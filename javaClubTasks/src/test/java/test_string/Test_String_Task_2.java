10765933d8ae5ca61610c317a96f3748e25bc29b
Міха <eglet777@ukr.net>
2021-11-26 20:39:04 +0200

    all test cases

--- javaClubTasks/src/test/java/test_string/Test_String_Task_2.java
+++ javaClubTasks/src/test/java/test_string/Test_String_Task_2.java
@@ -1,4 +1,11 @@
 package test_string;
+import org.junit.Test;
+import string.String_Task_2;
+import static org.junit.Assert.assertEquals;
 
 public class Test_String_Task_2 {
+    @Test
+    public void sampleTests() {
+        assertEquals("dlrow", String_Task_2.solution("world"));
+    }
 }
