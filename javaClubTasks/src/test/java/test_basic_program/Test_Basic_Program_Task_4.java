1bb387d7db8833a8072ba585785496e997fc5c75
Vitalii Sumka <vovk2001.ua@gmail.com>
2021-11-26 12:02:45 +0100

    added JUnit dependency in pom.xml

--- javaClubTasks/src/test/java/test_basic_program/Test_Basic_Program_Task_4.java
+++ javaClubTasks/src/test/java/test_basic_program/Test_Basic_Program_Task_4.java
@@ -1,5 +1,16 @@
 package test_basic_program;
+import basic_program.Basic_Program_Task_4;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 
 public class Test_Basic_Program_Task_4 {
-
+    @Test
+    public void FixedTests() {
+        assertEquals(18, Basic_Program_Task_4.century(1705));
+        assertEquals(19, Basic_Program_Task_4.century(1900));
+        assertEquals(17, Basic_Program_Task_4.century(1601));
+        assertEquals(20, Basic_Program_Task_4.century(2000));
+        assertEquals(1,  Basic_Program_Task_4.century(89));
+    }
 }
--- javaClubTasks/src/test/java/test_string/Test_String_Task_17.java
+++ javaClubTasks/src/test/java/test_string/Test_String_Task_17.java
@@ -1,4 +1,7 @@
 package test_string;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
 
 public class Test_String_Task_17 {
 }
