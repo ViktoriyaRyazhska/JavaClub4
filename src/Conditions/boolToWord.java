e641b3ff6baa4d1aa9670b84c7ae16f6f370668d
Taras Repak <taras.repak@gmail.com>
2021-11-27 17:51:22 +0200

    Create boolToWord.java

---
+++ src/Conditions/boolToWord.java
@@ -0,0 +1,13 @@
+package conditions;
+
+import java.util.Scanner;
+
+public class BooleanToString {
+    public static String convert(boolean b){
+      if(b) {
+        return "Yes";
+      } else {
+        return "No";
+      }
+    }
+}
