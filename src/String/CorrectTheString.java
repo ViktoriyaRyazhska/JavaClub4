78f3b6d1bcd41236dae92601300b70ff9e8e8f41
Taras Repak <taras.repak@gmail.com>
2021-11-27 18:50:05 +0200

    Create CorrectTheString.java

---
+++ src/String/CorrectTheString.java
@@ -0,0 +1,12 @@
+package String;
+
+import java.util.Scanner;
+
+public class CorrectTheString {
+  public static String correct(String string) {
+    string = string.replace("1", "I");
+    string = string.replace("0", "O");
+    string = string.replace("5", "S");
+    return string;
+  }
+}
