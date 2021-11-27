26076a1bdda806da994a7d6cf6fd772c5854541f
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-27 19:46:32 +0200

    completedtaask CorrectTheMistakes in package string

--- src/application/tasks/string/CorrectTheMistakes.java
+++ src/application/tasks/string/CorrectTheMistakes.java
@@ -1,7 +1,29 @@
 package application.tasks.string;
 
+import java.util.Scanner;
+
 public class CorrectTheMistakes {
     public static String correct(String string) {
-        return string;
+        StringBuilder builder = new StringBuilder();
+        String array[] = string.split("");
+        int count = 0;
+
+        for (String letter : array) {
+            if (letter.matches("[A-Z]")) {
+                builder.append(letter);
+                count++;
+            }
+            if (count == 1) {
+                builder.append(".");
+                count++;
+            }
+        }
+        return builder.toString();
+    }
+
+    private static void task(Scanner in) {
+        System.out.println("Enter any first and last name, so each word starts with capital letter");
+        String name = in.nextLine();
+        correct(name);
     }
 }
