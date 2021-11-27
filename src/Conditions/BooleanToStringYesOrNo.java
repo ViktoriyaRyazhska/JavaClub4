79dc01de0a27b2c36cf15a842a5c957b193c5624
Taras Repak <taras.repak@gmail.com>
2021-11-27 18:49:52 +0200

    Create BooleanToStringYesOrNo.java

---
+++ src/Conditions/BooleanToStringYesOrNo.java
@@ -0,0 +1,13 @@
+package Conditions;
+
+import java.util.Scanner;
+
+public class BooleanToStringYesOrNo {
+    public static String convert(boolean b){
+      if(b) {
+        return "Yes";
+      } else {
+        return "No";
+      }
+    }
+}
