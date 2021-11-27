fb952100e6dd50be30bc73dbfbb4d987c4cfef89
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-27 19:57:27 +0200

    completed task PushAnObjectIntoArray in the package collections

--- src/application/tasks/collections/PushAnObjectIntoArray.java
+++ src/application/tasks/collections/PushAnObjectIntoArray.java
@@ -2,11 +2,15 @@ package application.tasks.collections;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PushAnObjectIntoArray {
     public static List<String> push(){
         List<String> items = new ArrayList<>();
         items.add("an object");
         return items;
+    }
+    public static void task(Scanner in) {
+        System.out.println(push());
     }
 }
