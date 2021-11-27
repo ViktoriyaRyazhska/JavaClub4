a68763a6bd017b2f29c6f21c5f6888468d46bc98
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-27 19:58:53 +0200

    completed task Dictionary in the package collections

--- src/application/tasks/collections/Dictionary.java
+++ src/application/tasks/collections/Dictionary.java
@@ -1,16 +1,37 @@
 package application.tasks.collections;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Dictionary {
-    public Dictionary(){
-        // your code
+    public HashMap<String, String> hashMap;
+
+    public Dictionary(String key, String value) {
+        this.hashMap = new HashMap<>() {{
+            put(key, value);
+        }};
     }
 
-    public void newEntry(String key, String value){
+    public void newEntry(String key, String value) {
         // your code
+        hashMap.put(key, value);
     }
 
-    public String look(String key){
+    public String look(String key) {
         // your code
-        return "";
+        return hashMap.get(key);
+
+    }
+
+    public static void task(Scanner in) {
+
+        System.out.println("Add word to the dictionary: ");
+        String key = in.nextLine();
+        System.out.println("Add definition of the word: ");
+        String value = in.nextLine();
+
+        Dictionary dictionary = new Dictionary(key,value);
+        dictionary.newEntry(key, value);
+        System.out.println(dictionary.look(key));
     }
 }
