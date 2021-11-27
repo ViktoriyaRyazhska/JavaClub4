959643d41795806750101a0f277f55a55a9ed942
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-27 19:55:30 +0200

    completed task MyHeadIsAtTheWrongEnd in the package loops

--- src/application/tasks/loops/MyHeadIsAtTheWrongEnd.java
+++ src/application/tasks/loops/MyHeadIsAtTheWrongEnd.java
@@ -1,8 +1,51 @@
 package application.tasks.loops;
 
-public class MyHeadIsAtTheWrongEnd {
-    public static String[] fixTheMeerkat(String[] arr) {
-    return null;
-}
+import java.util.Scanner;
 
+public class MyHeadIsAtTheWrongEnd {
+    public static String[] fixAnimals(String[] arr) {
+        String temp;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("body") && i == 0) {
+                temp = arr[i];
+                arr[i] = "head";
+                arr[(i + 1)] = temp;
+            } else if (arr[i].equals("tail") && i == 0) {
+                temp = arr[i];
+                arr[i] = "head";
+                arr[(i + 2)] = temp;
+            } else if (arr[i].equals("head") && i == 1) {
+                temp = arr[i];
+                arr[i] = "body";
+                arr[(i - 1)] = temp;
+            } else if (arr[i].equals("tail") && i == 1) {
+                temp = arr[i];
+                arr[i] = "tail";
+                arr[(i + 1)] = temp;
+            } else if (arr[i].equals("head") && i == 2) {
+                temp = arr[i];
+                arr[i] = "tail";
+                arr[(i - 2)] = temp;
+            } else if (arr[i].equals("body") && i == 2) {
+                temp = arr[i];
+                arr[i] = "tail";
+                arr[(i - 1)] = temp;
+            }
+        }
+        return arr;
+    }
+
+    public static void task(Scanner in) {
+
+        String array[] = new String[3];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Enter " + (i + 1) + " part of  the animal like head, tail or body");
+            array[i] = in.nextLine();
+        }
+        String array1[] = fixAnimals(array);
+        for (String part:array1) {
+            System.out.println(part);
+        }
+    }
 }
