c303dc6ebd9e02351e1787f0360c81efe82bff28
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-27 19:47:39 +0200

    completed task AllStarCodeChallenge in the package string

--- src/application/tasks/string/AllStarCodeChallenge.java
+++ src/application/tasks/string/AllStarCodeChallenge.java
@@ -1,8 +1,24 @@
 package application.tasks.string;
 
+import java.util.Scanner;
+
 public class AllStarCodeChallenge {
-    public static int strCount(String str, char letter) {
-        //write code here
-        return 1;
+    private static int strCount(String str, char letter) {
+        String array[] = str.split("");
+        int count = 0;
+        for (String letterArr : array) {
+            if(letterArr.charAt(0) == letter)
+            count++;
+        }
+        return count;
+    }
+
+    public static void task(Scanner in) {
+        System.out.println("Enter any word:");
+        String word = in.nextLine();
+        System.out.println("Enter a char you want to count in the \"" + word + "\" word:");
+        char letter = in.next().charAt(0);
+        System.out.println(strCount(word, letter));
+
     }
 }
