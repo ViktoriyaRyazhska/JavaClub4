09f5bd57d3d11767c4479d6fad8d4a8e4b2a64b2
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-27 19:41:33 +0200

    completed task RemoveExplanationMarks in package string

--- src/application/tasks/string/RemoveExclamationMarks.java
+++ src/application/tasks/string/RemoveExclamationMarks.java
@@ -1,11 +1,43 @@
 package application.tasks.string;
 
+import java.util.*;
+
 public class RemoveExclamationMarks {
 
-    public static String remove(String s, int n){
+    public static String remove(String s, int n) {
+        int count = 0;
+        String array[] = s.split("");
+        StringBuilder builder = new StringBuilder();
 
-        return s;
-
+        //replace all "!"
+        if (n >= 100) {
+            s = s.replace("!", "");
+            return s;
+        } else if (n < 100) {
+            for (int i = 0; i < array.length; i++) {
+                if (count >= n) {
+                    array[i].replace("!", "");
+                } else {
+                    break;
+                }
+            }
+        }
+        //output
+        return builder.toString();
     }
 
+    public static void task(Scanner in) {
+        System.out.println("Enter sentence  with \"!\":");
+        String sentence = in.nextLine();
+        System.out.println("How  many \"!\" to remove:");
+        int n = in.nextInt();
+        System.out.println(remove(sentence, n));
+    }
+
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        // RemoveExclamationMarks.task(scanner);
+        //  System.out.println(RemoveExclamationMarks.remove("!HI!", 1));
+
+    }
 }
