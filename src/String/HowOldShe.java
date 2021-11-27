bd4204a49254f7cbef1d0565be85bc324d569453
Taras Repak <taras.repak@gmail.com>
2021-11-27 18:52:34 +0200

    Update HowOldShe.java

--- src/String/HowOldShe.java
+++ src/String/HowOldShe.java
@@ -1,9 +1,9 @@
-package string;
+package String;
 
 import java.util.Scanner;
 
 
-public class CharProblem {
+public class HowOldShe {
   public static int howOld(final String herOld) {
     String clear = herOld.replaceAll("\\D+","");
     int result = Integer.parseInt(clear);
