163ec813bb6d7e915db542a7723e4c246b120c7d
Taras Repak <taras.repak@gmail.com>
2021-11-27 17:52:51 +0200

    Create rentalCarCost.java

---
+++ src/Conditions/rentalCarCost.java
@@ -0,0 +1,20 @@
+package conditions;
+
+import java.util.Scanner;
+
+public class Kata {
+    public static int rentalCarCost(int d) {
+      if(d>=7){
+          d=((d*40)-50);
+          return d;
+      }
+      else if((d>=3)&&(d<7)){
+          d=((d*40)-20);
+          return d;
+      }
+      else{
+          d=d*40;
+          return d;
+      }
+    }
+}
