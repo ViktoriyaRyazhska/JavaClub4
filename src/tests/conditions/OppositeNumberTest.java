04a175710776869463565fb0b5824df2b50644cc
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-28 14:01:49 +0200

    added test OppositeNumberTestto tjhe package tests/conditions

---
+++ src/tests/conditions/OppositeNumberTest.java
@@ -0,0 +1,15 @@
+package tests.conditions;
+
+import application.tasks.conditions.OppositeNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OppositeNumberTest {
+    OppositeNumber oppositeNumber = new OppositeNumber();
+
+    @Test
+    void OppositeNumberTest() {
+        assertEquals(-1,oppositeNumber.getOpposite(1));
+    }
+}
