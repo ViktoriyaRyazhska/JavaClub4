006533d10d6dd1084b03ccef2b970f1246a55351
Nazar-Spodar <nazar.spodar38@gmail.com>
2021-11-28 19:32:31 +0200

    fraction

--- src/JUnitTests/Classes/TestFractionClass.java
+++ src/JUnitTests/Classes/TestFractionClass.java
@@ -2,13 +2,19 @@ package JUnitTests.Classes;
 
 import Tasks.Classes.FractionClass.FractionClassImpl;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class TestFractionClass {
-    FractionClassImpl fractionClass = new FractionClassImpl();
+    FractionClassImpl fractionClass1 = new FractionClassImpl(2, 0.0, true);
+    FractionClassImpl fractionClass2 = new FractionClassImpl(3, 0.5, true);
 
-//    @Test
-//    public void testFractionClass() {
-//        assertEquals(-17.0, fractionClass.add(2.0, 3.7));
-//    }
+    @Test
+    public void testFractionClass() {
+        assertThat(fractionClass1.add(fractionClass2).toDouble(), is(5.5));
+        assertThat(fractionClass1.differ(fractionClass2).toDouble(), is(-1.5));
+        assertThat(fractionClass1.multi(fractionClass2).toDouble(), is(7.0));
+        assertThat(fractionClass1.divide(fractionClass2).toDouble(), is(0.5714285714285714));
+
+    }
 }
