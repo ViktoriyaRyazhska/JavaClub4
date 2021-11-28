f801b34232a2f0177a1b7e5373ef8eb7ed9a4862
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-28 15:48:30 +0200

    added test WillBeEnoughSpaceTest t the package tests/conditions

---
+++ src/tests/conditions/WillBeEnoughSpaceTest.java
@@ -0,0 +1,16 @@
+package tests.conditions;
+
+import application.tasks.conditions.WillBeEnoughSpace;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WillBeEnoughSpaceTest {
+WillBeEnoughSpace  willBeEnoughSpace  = new WillBeEnoughSpace();
+    @Test
+    void willBeEnoughSpaceTest() {
+        assertEquals("Can fit all 5 passengers",willBeEnoughSpace.getOutPut(10,5,5));
+        assertEquals("Can fit all 5 passengers",willBeEnoughSpace.getOutPut(20,5,5));
+        assertEquals("Can`t fit 10 of the 50 passengers",willBeEnoughSpace.getOutPut(100,60,50));
+    }
+}
