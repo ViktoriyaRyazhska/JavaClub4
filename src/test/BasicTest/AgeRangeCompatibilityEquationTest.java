eee346ae0b7cdeeae3df248fb7864e093fb8cb38
Oksana <marjash@i.ua>
2021-11-30 00:03:56 +0200

    added tests

---
+++ src/test/BasicTest/AgeRangeCompatibilityEquationTest.java
@@ -0,0 +1,14 @@
+package test.BasicTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import Basic.*;
+
+class AgeRangeCompatibilityEquationTest {
+
+    @Test
+    void compatibilityEquation() {
+        //assertEquals("15-20", AgeRangeCompatibilityEquation.CompatibilityEquation(17));
+    }
+}
---
+++ src/test/ConditionsTest/CreateListTest.java
@@ -0,0 +1,21 @@
+package test.ConditionsTest;
+
+import Collections.CreateList;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateListTest {
+
+    @Test
+    void createList() {
+        assertEquals(Arrays.asList(1), CreateList.createList(1));
+        assertEquals(Arrays.asList(1, 2), CreateList.createList(2));
+        assertEquals(Arrays.asList(1, 2, 3), CreateList.createList(3));
+        assertEquals(Arrays.asList(1, 2, 3, 4), CreateList.createList(4));
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5), CreateList.createList(5));
+
+    }
+}
