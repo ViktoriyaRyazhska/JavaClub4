4beb8c8e308590a8017a7ee43e916297a5bbcbf9
Taras Repak <taras.repak@gmail.com>
2021-11-29 14:43:57 +0200

    Update DoIGetBonus.java

--- src/Conditions/DoIGetBonus.java
+++ src/Conditions/DoIGetBonus.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class DoIGetBonus{
     public static String bonusTime(final int salary, final boolean bonus) {
       if(bonus){
-          salary=salary*10;
-          return("\u00A3"+salary);
+        int n = salary * 10;
+        return("\u00A3"+n);
       }
       else{
         return("\u00A3"+salary);
