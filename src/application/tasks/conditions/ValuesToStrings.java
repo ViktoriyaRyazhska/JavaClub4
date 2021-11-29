dcf625f82a0bb5ad5c29f7f445b2723aaaec3d78
yura-stefanyshyn <yuristefanyshyn@gmail.com>
2021-11-29 18:19:04 +0200

    fixed class ValuesToStrings

--- src/application/tasks/conditions/ValuesToStrings.java
+++ src/application/tasks/conditions/ValuesToStrings.java
@@ -3,12 +3,11 @@ package application.tasks.conditions;
 import java.util.Scanner;
 
 public class ValuesToStrings {
-    public static String task(boolean b){
+    public static String task(boolean b,Scanner in){
         String Yes ="Yes";
         String No ="No";
-        Scanner bool = new Scanner(System.in);
         System.out.println("Your value is " + b);
-        b = bool.nextBoolean();
+        b = in.nextBoolean();
         if(b == true){
             return Yes;
         }else
