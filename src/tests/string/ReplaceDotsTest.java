c84900d7961674aceff32e821aff3fd1d8c0d348
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-29 18:33:21 +0200

    added ReplaceDotsTest to the package tests/string

---
+++ src/tests/string/ReplaceDotsTest.java
@@ -0,0 +1,14 @@
+package tests.string;
+
+import application.tasks.string.ReplaceDots;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReplaceDotsTest {
+    ReplaceDots replaceDots = new ReplaceDots();
+    @Test
+    void replaceDotsTest() {
+        assertTrue("one-two-three".equals(replaceDots.getReplacedDots("one.two.three")), "Sorry, try again :-(");
+    }
+}
