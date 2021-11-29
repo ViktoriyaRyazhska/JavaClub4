ee2fa9d23774290b76476284f767974ce7802620
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-29 18:26:24 +0200

    added BanjoTest  to the package tests/string

---
+++ src/tests/string/BanjoTest.java
@@ -0,0 +1,16 @@
+package tests.string;
+
+import application.tasks.string.Banjo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BanjoTest {
+    Banjo banjo = new Banjo();
+    @Test
+    void banjoTest() {
+        assertEquals("Martin does not play banjo",banjo.getArePlaying("Martin"));
+        assertEquals("Rikke plays banjo",banjo.getArePlaying("Rikke"));
+        assertEquals("rikke plays banjo",banjo.getArePlaying("rikke"));
+    }
+}
