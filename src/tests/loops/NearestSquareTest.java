07bd3b78849142a76a0b28681d63e13ad32d28dc
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-29 18:24:45 +0200

    added tests for the loops task package

---
+++ src/tests/loops/NearestSquareTest.java
@@ -0,0 +1,19 @@
+package tests.loops;
+
+import application.tasks.loops.NearestSquare;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NearestSquareTest {
+    NearestSquare nearestSquare = new NearestSquare();
+
+    @Test
+    void nearestSquareTest() {
+        assertEquals(1, nearestSquare.getFindSquareNumber(1));
+        assertEquals(1, nearestSquare.getFindSquareNumber(2));
+        assertEquals(9, nearestSquare.getFindSquareNumber(10));
+        assertEquals(121, nearestSquare.getFindSquareNumber(111));
+        assertEquals(10000, nearestSquare.getFindSquareNumber(9999));
+    }
+}
