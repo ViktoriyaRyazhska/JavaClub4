60fa346ec64be609b8a4a1815fe2ec64e2c1c003
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-29 18:54:55 +0200

    added WelcomeTest to the package tests/string

---
+++ src/tests/string/WelcomeTest.java
@@ -0,0 +1,20 @@
+package tests.string;
+
+import application.tasks.string.Welcome;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class WelcomeTest {
+    Welcome welcome = new Welcome();
+    List<String> name = Arrays.asList("John", "Smith");
+
+    @Test
+    void welcomeTest() {
+        assertEquals("Hello, John Smith! Welcome to Phoenix, Arizona!", welcome.getWelcome("Phoenix", "Arizona", name)
+        );
+    }
+}
