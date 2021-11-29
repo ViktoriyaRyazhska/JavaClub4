68e4d9a5027916787556d0f09c9f8a793cfcfc2d
yura-stefanyshyn <yuristefanyshyn@gmail.com>
2021-11-29 16:45:47 +0200

    fixed class CenturyFromYear

--- src/application/tasks/basic/CenturyFromYear.java
+++ src/application/tasks/basic/CenturyFromYear.java
@@ -2,17 +2,25 @@ package application.tasks.basic;
 import java.util.Scanner;
 
 public class CenturyFromYear {
-    private static void task(Scanner y){
-        System.out.println("Enter year");
-        int year;
-        year = y.nextInt();
-        if(year % 10 != 0){
-            year = (year / 100) + 1;
-            System.out.println("Your century is " + year + "th");
-        }else if (year % 10 == 0) {
-             year = year / 100;
-            System.out.println("Your century is " + year + "th");
-        }else
-            System.out.println("Enter year again");
+    public static void task(Scanner y) {
+        System.out.println("Enter your year");
+        int years = y.nextInt();
+        getCentury(years,y);
+    }
+        public static int getCentury(int years , Scanner y){
+            if (years % 10 != 0) {
+                years = (years / 100) + 1;
+                System.out.println("Your century is " + years + "th");
+                return years;
+            } else if (years % 10 == 0) {
+                years = years / 100;
+                System.out.println("Your century is " + years + "th");
+                return years;
+            } else {
+                System.out.println("Enter year again");
+            }
+                CenturyFromYear.task(y);
+            return 0;
+
     }
 }
