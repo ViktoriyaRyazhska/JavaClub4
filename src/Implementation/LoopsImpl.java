8ba7448fd62c75598f84c3810b449049661ba414
Nazarii Zobkiv <nazarii.v.zobkiv@gmail.com>
2021-11-29 22:07:04 +0200

    -

--- src/Implementation/LoopsImpl.java
+++ src/Implementation/LoopsImpl.java
@@ -2,6 +2,7 @@ package Implementation;
 
 import Loops.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -55,6 +56,7 @@ public class LoopsImpl {
     public static void MeanOfAnArray (Scanner scanner){
         System.out.println("Mean of an array");
         System.out.println("Введіть оцінки");
+<<<<<<< HEAD
 
         int grades;
         ArrayList<Integer> list = new ArrayList<>();
@@ -141,29 +143,57 @@ public class LoopsImpl {
         Scanner scanner = new Scanner(System.in);
         String string = scanner.next();
         System.out.println("Вкажіть кількість повторів");
+=======
+>>>>>>> b8607b9 (trying to save my work)
 
-        int repeat = scanner.nextInt();
-        System.out.println(Solution.repeatStr(repeat, string));
-        scanner.close();
+        int grades;
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter non integer to exit");
+        while (true){
+            if (scanner.hasNextInt()){
+                grades = scanner.nextInt();
+                list.add(grades);
+            } else {
+                break;
+            }
+        }
+        int[] arr = new int[list.size()];
+        for (int i = 0; i< arr.length; i++){
+            arr[i] = list.get(i);
+        }
+        System.out.println(MeanOfAnArray.getAverage(arr));
     }
 
-    public static void doSequence() {
-        System.out.println("reversedSequence");
-        System.out.println("Вкажіть число");
-        Scanner scanner = new Scanner(System.in);
-        int amount = scanner.nextInt();
-        System.out.println(Arrays.toString(Sequence.reverse(amount)));
-        scanner.close();
-    }
-    public static void twiceAsOld() {
-        System.out.println("twice-as-old");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введіть вік тата");
-        int fathersAge = scanner.nextInt();
-        System.out.println("Введіть вік сина");
-        int sonsAge = scanner.nextInt();
+    public static void ReduceButGrow (Scanner scanner) {
+        System.out.println("Reduce but grow");
+        System.out.println("Введіть значення");
 
-        System.out.println(TwiceAsOld.twiceAsOld(fathersAge,sonsAge));
-        scanner.close();
+        int values;
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter non integer to exit");
+        while (true){
+            if (scanner.hasNextInt()){
+                values = scanner.nextInt();
+                list.add(values);
+            } else {
+                break;
+            }
+        }
+        int[] arr = new int[list.size()];
+        for (int i = 0; i< arr.length; i++){
+            arr[i] = list.get(i);
+        }
+        System.out.println(ReduceButGrow.grow(arr));
     }
+<<<<<<< HEAD
+=======
+
+    public static void GrassHopper (Scanner scanner) {
+        System.out.println("Grass hopper");
+        System.out.println("Введіть значення");
+
+        int n = scanner.nextInt();
+        System.out.println(GrassHopper.summation(n));
+    }
+>>>>>>> b8607b9 (trying to save my work)
 }
