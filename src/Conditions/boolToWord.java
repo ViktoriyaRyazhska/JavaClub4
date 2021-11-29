65a072eb3167529ac2f3c353cc1ae8f02e8fe3d1
Nazarii Zobkiv <nazarii.v.zobkiv@gmail.com>
2021-11-29 15:59:11 +0200

    some fixes

--- src/Conditions/boolToWord.java
+++ src/Conditions/boolToWord.java
@@ -1,13 +1,13 @@
-//package Conditions;
-//
-//import java.util.Scanner;
-//
-//public class boolToWord {
-//    public static String convert(boolean b){
-//      if(b) {
-//        return "Yes";
-//      } else {
-//        return "No";
-//      }
-//    }
-//}
+package Conditions;
+
+import java.util.Scanner;
+
+public class boolToWord {
+    public static String convert(boolean b){
+      if(b) {
+        return "Yes";
+      } else {
+        return "No";
+      }
+    }
+}
