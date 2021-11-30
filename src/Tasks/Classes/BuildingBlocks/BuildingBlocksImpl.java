c3f47d9f55f296f6c8a5be0bd9c7b12946f24498
maksdaniluk140@gmail.com <maksym.danyliuk.itis.2020@lpnu.ua>
2021-11-30 05:34:47 +0200

    Fixed problem

--- src/Tasks/Classes/BuildingBlocks/BuildingBlocksImpl.java
+++ src/Tasks/Classes/BuildingBlocks/BuildingBlocksImpl.java
@@ -1,3 +1,55 @@
 package Tasks.Classes.BuildingBlocks;
 
+import java.util.Scanner;
 
+public class BuildingBlocksImpl implements BuildingBlocks {
+    private int width;
+    private int length;
+    private int height;
+
+    public BuildingBlocksImpl(final Scanner scanner) {
+        setWidth(scanner.nextInt());
+        setLength(scanner.nextInt());
+        setHeight(scanner.nextInt());
+    }
+
+    public BuildingBlocksImpl(final int width, final int length, final int height) {
+        setWidth(width);
+        setLength(length);
+        setHeight(height);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(final int width) {
+        this.width = Math.max(width, 1);
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(final int length) {
+        this.length = Math.max(length, 2);
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(final int height) {
+        this.height = Math.max(height, 1);
+    }
+
+    @Override
+    public int getVolume() {
+        return width * length * height;
+    }
+
+    @Override
+    public int getSurfaceArea() {
+        return 2 * (getWidth() * getLength() + getWidth() * getHeight() + getLength() * getHeight());
+    }
+}
