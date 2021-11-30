d663d2da67e24705241224aec34eba127357bdbe
Roman Kaniuka <kanuka878@gmail.com>
2021-11-30 14:38:51 +0200

    Uncommented

--- src/tests/tasks/string/CorrectTheMistakesTest.java
+++ src/tests/tasks/string/CorrectTheMistakesTest.java
@@ -9,12 +9,7 @@ class CorrectTheMistakesTest {
     CorrectTheMistakes correctTheMistakes = new CorrectTheMistakes();
     @Test
     void correctTest() {
-<<<<<<< HEAD
-   //     assertEquals("Hello, world", correctTheMistakes.getCorrect("Hell0, W0rld"), "Hell0, W0rld");
- //       assertEquals("1 am w0rld", correctTheMistakes.getCorrect("1 am w0rld"), "1 am w0rld");
-=======
-       // assertEquals("Hello, world", correctTheMistakes.getCorrect("Hell0, W0rld"), "Hell0, W0rld");
-        // assertEquals("1 am w0rld", correctTheMistakes.getCorrect("1 am w0rld"), "1 am w0rld");
->>>>>>> refs/remotes/origin/Team5
+        assertEquals("HellO, WOrld", correctTheMistakes.getCorrect("Hell0, W0rld"), "Hell0, W0rld");
+        assertEquals("I am wOrld", correctTheMistakes.getCorrect("1 am w0rld"), "1 am w0rld");
     }
 }
