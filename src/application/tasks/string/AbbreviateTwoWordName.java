8a2f0f1c10aa2d81c86a2f5d4ccdc4a45e38aa1a
DavidNazariy <davidnazariykrupka@gmail.com>
2021-11-30 15:40:14 +0200

    fixed AbbreviateTwoWordName task in the package string

--- src/application/tasks/string/AbbreviateTwoWordName.java
+++ src/application/tasks/string/AbbreviateTwoWordName.java
@@ -1,5 +1,6 @@
 package application.tasks.string;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AbbreviateTwoWordName {
@@ -9,21 +10,25 @@ public class AbbreviateTwoWordName {
 
     public static String correct(String string) {
         StringBuilder builder = new StringBuilder();
-        String array[] = string.split("");
+        //splits  string  into two words array
+        String array[] = string.split(" ");
         int count = 0;
-
-        for (String letter : array) {
-            if (letter.matches("[A-Z]")) {
-                builder.append(letter);
-                count++;
-            }
-            if (count == 1) {
+        //splits each word
+        for (String word : array) {
+            String array1[] = word.split("");
+            //add . between name  and surname
+            if (count > 0)
                 builder.append(".");
-                count++;
+            for (String letter : array1) {
+                if (letter.matches("[a-zA-Z]") && letter.matches(array1[0])) {
+                    builder.append(letter.toUpperCase(Locale.ROOT));
+                    count++;
+                }
             }
         }
         return builder.toString();
     }
+
     public String getCorrect(String string) {
         return correct(string);
     }
@@ -33,5 +38,10 @@ public class AbbreviateTwoWordName {
         System.out.println("Enter any first and last name, each word starts with capital letter");
         String name = in.nextLine();
         System.out.println("Abbreviation for the " + name + " is: " + correct(name));
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        AbbreviateTwoWordName.task(in);
     }
 }
