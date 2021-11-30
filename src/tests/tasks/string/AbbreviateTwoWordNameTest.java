5935d9528f2b2b1be3c6313f401192c6158d2216
Roman Kaniuka <kanuka878@gmail.com>
2021-11-30 12:03:56 +0200

    Added test "AbbreviateTwoWordNameTest" to package 'tests/tasks/string'

---
+++ src/tests/tasks/string/AbbreviateTwoWordNameTest.java
@@ -0,0 +1,16 @@
+package tests.tasks.string;
+
+import application.tasks.string.AbbreviateTwoWordName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbbreviateTwoWordNameTest {
+    AbbreviateTwoWordName object = new AbbreviateTwoWordName();
+    @Test
+    void correctTest() {
+        assertEquals("H.W", object.getCorrect("hello world"), "hello world");
+        assertEquals("H.W", object.getCorrect("Hello World"), "Hello World");
+        assertEquals("", object.getCorrect("hello, world"), "hello, world");
+    }
+}
