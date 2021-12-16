ce43c9e8ed4f5bb6b3aaad34a8b6b02d659aab5d
Vitalii Konovart <konovart.vitalik@gmail.com>
2021-12-16 20:46:16 +0200

    Added book repository

---
+++ src/main/java/org/softserve/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package org.softserve.repository;
+
+import org.softserve.model.Book;
+
+public interface BookRepository {
+    Book create(Book book);
+
+    void read(int id);
+
+    void update(Book book);
+
+    void delete(int id);
+}
---
+++ src/main/java/org/softserve/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,25 @@
+package org.softserve.repository.impl;
+
+import org.softserve.model.Book;
+import org.softserve.repository.BookRepository;
+public class BookRepositoryImpl implements BookRepository{
+    @Override
+    public Book create(Book book) {
+        return null;
+    }
+
+    @Override
+    public void read(int id) {
+
+    }
+
+    @Override
+    public void update(Book book) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
